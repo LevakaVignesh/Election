fdc68534dc42bbe1226dfbd37bdb624ad4ebcb11
vignesh <vigneshlevaka05@gmail.com>
2022-05-03 08:20:39 +0530

    first commit

---
+++ src/main/resources/data.sql
@@ -0,0 +1,27 @@
+insert into Credential_details values('admin123',0,'admin','A');
+
+
+insert into Election_details values('101','kadapa','2000-03-20','kadapa','2022-04-20','vignesh');
+
+
+insert into Party_details values('1','leader','vignesh','@');
+insert into Party_details values('2','coleader','sravya','$');
+insert into Party_details values('3','leader','srija','*');
+insert into Party_details values('4','coleader','vasavi','!');
+insert into Party_details values('5','leader','srijan','&');
+insert into Party_details values('6','coleader','swapna','+');
+
+
+insert into Candidate_details values('10','maruthi nagar','kadapa','2000-09-27','kadapa','vignesh@gmail','7484766123','vignesh','101','1');
+
+
+insert into Application_details values(1,'kadapa',2,'101','admin123');
+
+
+insert into profile_details values('kadapa','2000-09-27','vignesh@gmail.com','levaka','Male','Vignesh','Kadapa','123456789','pw123','516001','Andhra','maruthinagar','admin123');
+
+
+insert into Result_details values(1,'10','101',501,'10','101');
+
+
+insert into Eo_details values('101','kadapa');
