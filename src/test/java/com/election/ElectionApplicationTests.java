e233ad65a8609d4a1762026162ba6c89f6a74320
Levaka Vignesh <86885479+LevakaVignesh@users.noreply.github.com>
2022-05-04 23:01:25 +0530

    Update ElectionApplicationTests.java

--- src/test/java/com/election/ElectionApplicationTests.java
+++ src/test/java/com/election/ElectionApplicationTests.java
@@ -2,8 +2,7 @@ package com.election;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
+@SpringBootTest(classes = FlightReservationSystemApplication.class)
 class ElectionApplicationTests {
 
 	@Test
