ebaeb27a8f6a21ad5a60b1376aad33938acf999b
Levaka Vignesh <86885479+LevakaVignesh@users.noreply.github.com>
2022-05-05 16:25:54 +0530

    Update ElectionApplicationTests.java

--- src/test/java/com/election/ElectionApplicationTests.java
+++ src/test/java/com/election/ElectionApplicationTests.java
@@ -5,8 +5,8 @@ import org.springframework.boot.SpringBootConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ContextConfiguration;
 @SpringBootConfiguration
-@ContextConfiguration
 @SpringBootTest
+@ContextConfiguration
 
 class ElectionApplicationTests {
 
